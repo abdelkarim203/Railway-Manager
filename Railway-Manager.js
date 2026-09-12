eb77102413a33f968187c91ab3d0bf47dc94d362
abdelkarim203 <karimmarzouk92@gmail.com>
2026-09-12 10:12:02 +0100

    .\Railway-Manager.js

--- Railway-Manager.js
+++ Railway-Manager.js
@@ -217,13 +217,9 @@ do {
         case 7: trierTrajets();
             break;
         case 0: quitter();
-<<<<<<< HEAD
-=======
-            console.log("Au revoir ");
->>>>>>> b1cf0325c43ead71d89b9b62af42b7ac805db3c6
             break;
         default:
-            console.log("Choix invalide.");
+            console.log('\n' + "Choix invalide." + '\n');
     }
 } while (choice !== 0)
 function AffichageTrajets() {
@@ -315,32 +311,19 @@ function annulationTicket() {
     identifiant = Number(prompt('Identifiant du ticket : '));
     for (let i = 0; i < tickets.length; i++) {
         if (tickets[i].id === identifiant) {
-<<<<<<< HEAD
-            for (j=0 ; j < trips.length ; j++){
-                if (tickets[i].tripId == trips[j].id){
+            tickets.splice(identifiant - 1, 1)
+            for (j = 0; j < trips.length; j++) {
+                if (tickets[i].tripId == trips[j].id) {
                     ++trips[j].availableSeats
                     break;
                 }
             }
-            tickets.splice(i, 1)
-            console.log("ticket annule avec succes");
-            return;
-            
-=======
-            tickets.splice(identifiant - 1, 1)
-            ++trips[i].availableSeats
-            break;
->>>>>>> b1cf0325c43ead71d89b9b62af42b7ac805db3c6
         }
 
     }
 
 
-<<<<<<< HEAD
-    console.log('\n'+"ticket introvable"+'\n');
-=======
     return "ticket introvable"
->>>>>>> b1cf0325c43ead71d89b9b62af42b7ac805db3c6
 }
 
 
@@ -394,20 +377,6 @@ function filtrerTrajets() {
     let ville = prompt('entrer ville de depart : ');
     for (let i = 0; i < trips.length; i++) {
         if (ville == trips[i].departure.toLowerCase()) {
-<<<<<<< HEAD
-
-            console.log(ville + "-->" + trips[i].destination);
-
-        }
-
-    }
-    return;
-
-}
-function quitter(){
-    console.log("Au revoir ");
-}
-=======
 
             console.log(ville + "-->" + trips[i].destination);
 
@@ -418,6 +387,8 @@ function quitter(){
 
 }
 
+function quitter() {
+    console.log('\n' + "Au revoir " + '\n');
 
+}
 
->>>>>>> b1cf0325c43ead71d89b9b62af42b7ac805db3c6
